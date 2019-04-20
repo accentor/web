686066e7f97024d4a02f445e6c4bc7379e21acd5
Robbe Van Petegem <robbe@vanpetegem.me>
2019-04-20 10:54:38 +0000

    Add settings panel for current user

---
+++ src/views/settings/Settings.vue
@@ -0,0 +1,71 @@
+<template>
+  <VContainer fill-height fluid v-if="user">
+    <VLayout align-center justify-center>
+      <VFlex md4 sm8 xs12>
+        <VForm @submit.prevent="submit">
+          <VTextField label="Name" v-model="newUser.name" />
+          <VTextField
+            label="Password"
+            type="password"
+            v-model="newUser.password"
+          />
+          <VTextField
+            label="Confirm password"
+            type="password"
+            v-model="newUser.password_confirmation"
+          />
+          <VBtn color="primary" type="submit">Update user</VBtn>
+        </VForm>
+      </VFlex>
+    </VLayout>
+  </VContainer>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  name: "EditUser",
+  data() {
+    return {
+      newUser: {
+        name: "",
+        password: "",
+        password_confirmation: ""
+      }
+    };
+  },
+  created() {
+    setTimeout(() => {
+      if (this.user) {
+        this.fillValues();
+      }
+    });
+  },
+  watch: {
+    user: function() {
+      if (this.user) {
+        this.fillValues();
+      }
+    }
+  },
+  computed: {
+    ...mapGetters("auth", { user: "currentUser" })
+  },
+  methods: {
+    ...mapActions("users", ["update"]),
+    fillValues() {
+      this.newUser.name = this.user.name;
+    },
+    submit() {
+      this.update({ id: this.user.id, newUser: this.newUser }).then(
+        succeeded => {
+          if (succeeded) {
+            this.$router.push(this.$route.query.redirect || { name: "home" });
+          }
+        }
+      );
+    }
+  }
+};
+</script>
