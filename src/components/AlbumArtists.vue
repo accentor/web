a06239ee0df0f05723ce8fcec0a06b57ffdd74d0
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-05-01 13:19:22 +0200

    Fix ordering of album artists

--- src/components/AlbumArtists.vue
+++ src/components/AlbumArtists.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <span v-for="aa of album.album_artists" :key="`${aa.artist_id} ${aa.name}`"
+    <span v-for="aa of album_artists" :key="`${aa.artist_id} ${aa.name}`"
       ><RouterLink :to="{ name: 'artist', params: { id: aa.artist_id } }">{{
         aa.name
       }}</RouterLink
@@ -13,6 +13,13 @@ export default {
   name: "AlbumArtists",
   props: {
     album: {}
+  },
+  computed: {
+    album_artists() {
+      return this.album.album_artists
+        .map(aa => aa)
+        .sort((a1, a2) => a1.order - a2.order);
+    }
   }
 };
 </script>
