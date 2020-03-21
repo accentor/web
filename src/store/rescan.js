87caad9a7c2e4010cc66408ab704fd01135abeed
robbevp <48474670+robbevp@users.noreply.github.com>
2020-03-21 17:40:04 +0100

    Improve rescan triggering (#45)
    * Make sure to keep refreshing until rescan finishes
    
    * trigger loadData after rescan finishes
    
    * Revert "trigger loadData after rescan finishes"
    
    This reverts commit 96487ea199fd58a7009960b007bb8a19302894f3.
    
    * rename last_scan to finished_At
    
    * lint --fix
    
    * Refresh data after a rescan finished
    
    * Fix missing icons
    
    Caused by the material design icons update
    
    * Make sure to keep refreshing until rescan finishes
    
    * rename last_scan to finished_At
    
    * lint --fix
    
    * seperate lastClick from rescan obj
    
    * Show Finished at or currently scanning
    
    * remove unused locale keys
    
    * don't set last_click in rescan obj
    
    * steLAstClick calling API
    
    * simplify mutation
    
    Co-authored-by: Charlotte Van Petegem <charlotte@vanpetegem.me>

--- src/store/rescan.js
+++ src/store/rescan.js
@@ -3,9 +3,13 @@ import { show, start } from "../api/rescan";
 export default {
   namespaced: true,
   state: {
-    rescan: null
+    rescan: null,
+    lastClick: new Date(0)
   },
   mutations: {
+    setLastClick(state, payload) {
+      state.lastClick = payload;
+    },
     setRescan(state, payload) {
       state.rescan = payload;
     }
@@ -15,7 +19,7 @@ export default {
       return show(rootState.auth)
         .then(result => {
           commit("setRescan", result);
-          if (result.running) {
+          if (rootState.rescan.lastClick > new Date(result.finished_at)) {
             setTimeout(() => dispatch("show"), 1000);
           }
           return Promise.resolve(true);
@@ -26,6 +30,7 @@ export default {
         });
     },
     start({ commit, dispatch, rootState }) {
+      commit("setLastClick", new Date());
       return start(rootState.auth)
         .then(result => {
           result.running = true;
--- src/views/Library.vue
+++ src/views/Library.vue
@@ -11,12 +11,28 @@
       <h2 class="headline">{{ $t("library.rescan") }}</h2>
     </VRow>
     <VRow v-if="rescan">
-      <VBtn @click="start" color="success" class="ma-2">
-        <VIcon left>mdi-refresh {{ rescan.running ? "mdi-spin" : "" }}</VIcon>
+      <VBtn
+        @click="start"
+        :disabled="rescanRunning"
+        color="success"
+        class="ma-2"
+      >
+        <VIcon left>
+          mdi-refresh
+          {{ rescanRunning ? "mdi-spin" : "" }}
+        </VIcon>
         {{ $t("library.start-scan") }}
       </VBtn>
     </VRow>
     <VRow class="flex-column" v-if="rescan">
+      <div>
+        <strong>{{ $t("library.finished-at") }}: </strong>
+        {{
+          rescanRunning
+            ? $t("library.currently-running")
+            : $d(new Date(rescan.finished_at), "long")
+        }}
+      </div>
       <div>
         <strong>{{ $t("library.processed") }}: </strong>
         {{ rescan.processed }}
@@ -100,7 +116,11 @@ export default {
   },
   computed: {
     ...mapGetters("auth", ["isModerator"]),
-    ...mapState("rescan", ["rescan"])
+    ...mapState("rescan", ["rescan"]),
+    ...mapState("rescan", ["lastClick"]),
+    rescanRunning() {
+      return this.lastClick > new Date(this.rescan.finished_at) ? true : false;
+    }
   },
   methods: {
     ...mapActions("rescan", ["start"])
