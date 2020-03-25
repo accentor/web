e50c77231a852787d525d250608537589cc236a3
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-03-25 18:15:25 +0100

    Allow overriding the base API url via the env (#60)

--- src/api/base_url.js
+++ src/api/base_url.js
@@ -1,5 +1,7 @@
 let url;
-if (process.env.NODE_ENV === "production") {
+if (process.env.VUE_APP_API_URL !== undefined) {
+  url = process.env.VUE_APP_API_URL;
+} else if (process.env.NODE_ENV === "production") {
   url = "/api";
 } else {
   url = "http://localhost:3000/api";
