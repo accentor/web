f10d4b83cd83054998803427a0d2c1654d51d9bd
Robbe Van Petegem <robbe@vanpetegem.me>
2020-07-12 16:15:17 +0200

    Expand actions inline rather than in a VMenu

--- src/components/TrackActions.vue
+++ src/components/TrackActions.vue
@@ -37,57 +37,48 @@
       <span>{{ $t("music.track.empty") }}</span>
     </VTooltip>
     <EditReviewComment :item="track" :update="flag" v-if="!isModerator" />
-    <VMenu v-model="expanded" offset-y v-else class="">
-      <template v-slot:activator="{ on }">
-        <span v-on="on">
-          <VBtn class="ma-1" color="edit" small icon>
-            <VIcon v-if="expanded">mdi-close</VIcon>
-            <VIcon v-else>mdi-arrow-expand-down</VIcon>
-          </VBtn>
-        </span>
-      </template>
-      <VList dense>
-        <VListItem class="px-0">
-          <VListContent class="py-0">
-            <EditReviewComment :item="track" :update="flag" />
-          </VListContent>
-        </VListItem>
-        <VListItem class="px-0">
-          <VListItemContent class="py-0">
-            <VBtn
-              :to="{
-                name: 'edit-track',
-                params: { id: track.id },
-                query: { redirect: $route.fullPath },
-              }"
-              :disabled="waitingForReload"
-              color="edit"
-              class="ma-1 flex-grow-0 flex-basis-auto"
-              text
-              icon
-              small
-            >
-              <VIcon>mdi-pencil</VIcon>
-            </VBtn>
-          </VListItemContent>
-        </VListItem>
-        <VLisstItem class="px-0">
-          <VListItemContent class="py-0">
-            <VBtn
-              @click.stop.prevent="deleteTrack"
-              :disabled="waitingForReload"
-              color="danger"
-              class="ma-1 flex-grow-0 flex-basis-auto"
-              text
-              icon
-              small
-            >
-              <VIcon>mdi-delete</VIcon>
-            </VBtn>
-          </VListItemContent>
-        </VLisstItem>
-      </VList>
-    </VMenu>
+    <VBtn
+      class="ma-1"
+      color="edit"
+      small
+      icon
+      @click.stop="expanded = !expanded"
+    >
+      <VIcon v-if="expanded">mdi-arrow-expand-left</VIcon>
+      <VIcon v-else>mdi-arrow-expand-right</VIcon>
+    </VBtn>
+    <VSlideXReverseTransition>
+      <span v-show="expanded">
+        <EditReviewComment :item="track" :update="flag" />
+        <VBtn
+          :to="{
+            name: 'edit-track',
+            params: { id: track.id },
+            query: { redirect: $route.fullPath },
+          }"
+          :disabled="waitingForReload"
+          color="edit"
+          class="ma-1 flex-grow-0 flex-basis-auto"
+          text
+          icon
+          small
+        >
+          <VIcon>mdi-pencil</VIcon>
+        </VBtn>
+
+        <VBtn
+          @click.stop.prevent="deleteTrack"
+          :disabled="waitingForReload"
+          color="danger"
+          class="ma-1 flex-grow-0 flex-basis-auto"
+          text
+          icon
+          small
+        >
+          <VIcon>mdi-delete</VIcon>
+        </VBtn>
+      </span>
+    </VSlideXReverseTransition>
   </span>
 </template>
 
