ea3543d33718ff633e727d02c782a1f0d0fb4002
Robbe Van Petegem <robbe@vanpetegem.me>
2020-07-26 12:52:09 +0200

    Add audiofile details to track actions

--- src/components/TrackActions.vue
+++ src/components/TrackActions.vue
@@ -105,6 +105,68 @@
           </template>
           <span>{{ $t("common.disabled-while-loading") }}</span>
         </VTooltip>
+        <VMenu open-on-hover offset-x left v-if="track.length">
+          <template v-slot:activator="{ on }">
+            <VListItem v-on="on">
+              <VListItemIcon v-on="on">
+                <VIcon>mdi-file-music</VIcon>
+              </VListItemIcon>
+              <VListItemContent>
+                <VListItemTitle>
+                  {{ $t("music.track.file") }}
+                </VListItemTitle>
+              </VListItemContent>
+            </VListItem>
+          </template>
+          <VList dense>
+            <VListItem v-if="Object.keys(locations).length > 1">
+              <VListItemContent>
+                <VListItemTitle> {{ $t("library.location") }}: </VListItemTitle>
+                <VListItemSubtitle>
+                  {{ locations[track.location_id].path }}
+                </VListItemSubtitle>
+              </VListItemContent>
+            </VListItem>
+            <VListItem>
+              <VListItemContent>
+                <VListItemTitle> {{ $t("library.filename") }}: </VListItemTitle>
+                <VListItemSubtitle>
+                  {{ track.filename }}
+                </VListItemSubtitle>
+              </VListItemContent>
+            </VListItem>
+            <VListItem>
+              <VListItemContent>
+                <VListItemTitle>
+                  {{ $t("music.track.bitrate") }}:
+                </VListItemTitle>
+                <VListItemSubtitle>
+                  {{ track.bitrate }}
+                </VListItemSubtitle>
+              </VListItemContent>
+            </VListItem>
+            <VListItem v-if="track.hasOwnProperty('sample_rate')">
+              <VListItemContent>
+                <VListItemTitle>
+                  {{ $t("music.track.sample_rate") }}:
+                </VListItemTitle>
+                <VListItemSubtitle>
+                  {{ track.sample_rate }}
+                </VListItemSubtitle>
+              </VListItemContent>
+            </VListItem>
+            <VListItem v-if="track.hasOwnProperty('bit_depth')">
+              <VListItemContent>
+                <VListItemTitle>
+                  {{ $t("music.track.bit_depth") }}:
+                </VListItemTitle>
+                <VListItemSubtitle>
+                  {{ track.bit_depth }}
+                </VListItemSubtitle>
+              </VListItemContent>
+            </VListItem>
+          </VList>
+        </VMenu>
       </VList>
     </VMenu>
   </span>
@@ -123,6 +185,8 @@ export default {
   computed: {
     ...mapGetters("auth", ["isModerator"]),
     ...mapState("tracks", ["startLoading"]),
+    ...mapState("codecs", ["codecs"]),
+    ...mapState("locations", ["locations"]),
     waitingForReload() {
       return this.startLoading > this.track.loaded;
     },
