e241cf4198811e07e689e3ea8c86b55acd48e5c3
Robbe Van Petegem <robbe@vanpetegem.me>
2020-08-03 21:17:13 +0200

    Add isModerator property

--- src/views/tracks/EmptyTracks.vue
+++ src/views/tracks/EmptyTracks.vue
@@ -22,6 +22,7 @@ export default {
   name: "EmptyTracks",
   components: { TracksTable },
   computed: {
+    ...mapGetters("auth", ["isModerator"]),
     ...mapGetters("tracks", {
       tracks: "tracksEmpty",
     }),
