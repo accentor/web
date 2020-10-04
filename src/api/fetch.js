b9bb245878c3fb97f6a745dc6c75d1a60797c071
Robbe Van Petegem <robbe@vanpetegem.me>
2020-10-04 12:16:00 +0200

    Switch to async syntax in API calls

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -35,101 +35,92 @@ export async function* indexGenerator(path, auth) {
   }
 }
 
+async function resolveRequest(asyncCallback) {
+  const request = await asyncCallback.catch((reason) => {
+    throw { error: [reason] };
+  });
+  const result = await request.json();
+  if (request.ok) {
+    return result;
+  } else {
+    throw result;
+  }
+}
+
 export function create(path, auth, object) {
-  return fetch(`${baseURL}/${path}`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-      "x-secret": auth.secret,
-      "x-device-id": auth.device_id,
-    },
-    body: JSON.stringify(object),
-  })
-    .catch((reason) => Promise.reject({ error: [reason] }))
-    .then((request) => Promise.all([request.ok, request.json()]))
-    .then(([ok, result]) => {
-      return ok ? Promise.resolve(result) : Promise.reject(result);
-    });
+  return resolveRequest(
+    fetch(`${baseURL}/${path}`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "x-secret": auth.secret,
+        "x-device-id": auth.device_id,
+      },
+      body: JSON.stringify(object),
+    })
+  );
 }
 
 export function read(path, auth) {
-  return fetch(`${baseURL}/${path}`, {
-    method: "GET",
-    headers: {
-      "x-secret": auth.secret,
-      "x-device-id": auth.device_id,
-    },
-  })
-    .catch((reason) => Promise.reject({ error: [reason] }))
-    .then((request) => Promise.all([request.ok, request.json()]))
-    .then(([ok, result]) => {
-      return ok ? Promise.resolve(result) : Promise.reject(result);
-    });
+  return resolveRequest(
+    fetch(`${baseURL}/${path}`, {
+      method: "GET",
+      headers: {
+        "x-secret": auth.secret,
+        "x-device-id": auth.device_id,
+      },
+    })
+  );
 }
 
 export function update(path, auth, object) {
-  return fetch(`${baseURL}/${path}`, {
-    method: "PATCH",
-    headers: {
-      "content-type": "application/json",
-      "x-secret": auth.secret,
-      "x-device-id": auth.device_id,
-    },
-    body: JSON.stringify(object),
-  })
-    .catch((reason) => Promise.reject({ error: [reason] }))
-    .then((request) => Promise.all([request.ok, request.json()]))
-    .then(([ok, result]) => {
-      return ok ? Promise.resolve(result) : Promise.reject(result);
-    });
+  return resolveRequest(
+    fetch(`${baseURL}/${path}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        "x-secret": auth.secret,
+        "x-device-id": auth.device_id,
+      },
+      body: JSON.stringify(object),
+    })
+  );
 }
 
-export function destroy(path, auth) {
-  return fetch(`${baseURL}/${path}`, {
-    method: "DELETE",
-    headers: {
-      "x-secret": auth.secret,
-      "x-device-id": auth.device_id,
-    },
-  })
-    .catch((reason) => Promise.reject({ error: [reason] }))
-    .then((request) => {
-      return request.ok
-        ? Promise.resolve()
-        : request.json().then((result) => Promise.reject(result));
-    });
+export async function destroy(path, auth) {
+  return resolveRequest(
+    fetch(`${baseURL}/${path}`, {
+      method: "DELETE",
+      headers: {
+        "x-secret": auth.secret,
+        "x-device-id": auth.device_id,
+      },
+    })
+  );
 }
 
 export function destroyEmpty(path, auth) {
-  return fetch(`${baseURL}/${path}/destroy_empty`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-      "x-secret": auth.secret,
-      "x-device-id": auth.device_id,
-    },
-  })
-    .catch((reason) => Promise.reject({ error: [reason] }))
-    .then((request) => {
-      return request.ok
-        ? Promise.resolve()
-        : request.json().then((result) => Promise.reject(result));
-    });
+  return resolveRequest(
+    fetch(`${baseURL}/${path}/destroy_empty`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "x-secret": auth.secret,
+        "x-device-id": auth.device_id,
+      },
+    })
+  );
 }
 
 export function merge(path, auth) {
-  return fetch(`${baseURL}/${path}`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-      "x-secret": auth.secret,
-      "x-device-id": auth.device_id,
-    },
-  })
-    .catch((reason) => Promise.reject({ error: [reason] }))
-    .then((request) => {
-      return request.ok
-        ? Promise.resolve()
-        : request.json().then((result) => Promise.reject(result));
-    });
+  return resolveRequest(
+    fetch(`${baseURL}/${path}`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "x-secret": auth.secret,
+        "x-device-id": auth.device_id,
+      },
+    })
+  );
 }
