37064b2432def38ff9cc238122e94602daae1f22
Robbe Van Petegem <robbe@vanpetegem.me>
2020-10-10 11:19:14 +0200

    Re-add eslint:recommended to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   plugins: ["vuetify"],
   rules: {
     "vuetify/no-deprecated-classes": "error",
