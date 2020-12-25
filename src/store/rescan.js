f169049b558b397f0a50eec6379d9cb8ebc9a191
robbevp <robbe@vanpetegem.me>
2020-12-25 13:56:27 +0100

    Prevent rescan show from starting multiple loops (#379)

--- src/store/rescan.js
+++ src/store/rescan.js
@@ -5,29 +5,40 @@ export default {
   state: {
     rescan: null,
     lastClick: new Date(0),
+    loading: false,
   },
   mutations: {
     setLastClick(state, payload) {
       state.lastClick = payload;
+    },
+    setLoading(state, payload) {
+      state.loading = payload;
     },
     setRescan(state, payload) {
       state.rescan = payload;
     },
   },
   actions: {
-    async show({ commit, dispatch, rootState }) {
+    async show({ commit, rootState }) {
+      if (rootState.rescan.loading) {
+        return true;
+      }
       try {
-        const result = await show(rootState.auth);
-        commit("setRescan", result);
-        if (
+        commit("setLoading", true);
+        let result = null;
+        do {
+          result = await show(rootState.auth);
+          commit("setRescan", result);
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        } while (
           rootState.rescan.lastClick > new Date(result.finished_at) ||
           result.running
-        ) {
-          setTimeout(() => dispatch("show"), 1000);
-        }
+        );
+        commit("setLoading", false);
         return true;
       } catch (error) {
         commit("addError", error, { root: true });
+        commit("setLoading", false);
         return false;
       }
     },
