4945ec325f9e89cc13c84e4327765101f82b8df1
Robbe Van Petegem <robbe@vanpetegem.me>
2021-07-11 16:51:39 +0200

    Rewrite scopes with method syntax

--- src/api/scopes.js
+++ src/api/scopes.js
@@ -5,52 +5,55 @@ export class Scope {
     return this.scopes.map((s) => `&${s.key}=${s.query}`).join("");
   }
 
-  addScope = (key, query) => {
+  addScope(key, query) {
     this.scopes.push({ key, query });
     return this;
-  };
+  }
 
-  addScopesFromArray = (key, queries) => {
+  addScopesFromArray(key, queries) {
     queries.forEach((q) => this.addScope(key, q));
     return this;
-  };
+  }
 }
 
 export class AlbumsScope extends Scope {
-  artist = (id) => {
+  artist(id) {
     return this.addScope("artist_id", id);
-  };
+  }
 
-  filter = (string) => {
+  filter(string) {
     return this.addScope("filter", string);
-  };
+  }
 
-  label = (id) => {
+  label(id) {
     return this.addScope("label", id);
-  };
+  }
 
-  labels = (ids) => {
+  labels(ids) {
     return this.addScopesFromArray("labels", ids);
-  };
+  }
 }
 
 export class ArtistsScope extends Scope {
-  filter = (string) => {
+  filter(string) {
     return this.addScope("filter", string);
-  };
+  }
 }
 
 export class TracksScope extends Scope {
-  album = (id) => {
+  album(id) {
     return this.addScope("album_id", id);
-  };
-  artist = (id) => {
+  }
+
+  artist(id) {
     return this.addScope("artist_id", id);
-  };
-  filter = (string) => {
+  }
+
+  filter(string) {
     return this.addScope("filter", string);
-  };
-  genre = (id) => {
+  }
+
+  genre(id) {
     return this.addScope("genre_id", id);
-  };
+  }
 }
