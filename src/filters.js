ca2e0f3c2006909beb553d15691d634464fdef05
Robbe Van Petegem <robbe@vanpetegem.me>
2021-11-28 13:17:05 +0100

    Lint

--- src/filters.js
+++ src/filters.js
@@ -1,7 +1,6 @@
 export function filterPlaysByPeriod(startDate, endDate) {
   return function (play) {
     const playedAt = new Date(play.played_at);
-    console.debug(startDate, playedAt, endDate);
     return playedAt > startDate && playedAt < endDate;
   };
 }
