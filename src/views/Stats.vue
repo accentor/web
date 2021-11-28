8d1ad4c2b806ad97b3308f93c92f42c1880668d1
Robbe Van Petegem <robbe@vanpetegem.me>
2021-11-28 13:06:39 +0100

    Add Stats page

---
+++ src/views/Stats.vue
@@ -0,0 +1,49 @@
+<template>
+  <VContainer>
+    <VRow>
+      <VCol>
+        <h1>{{ $t("common.stats") }}</h1>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+import DateRangeSelect from "@/components/DateRangeSelect";
+import PercentagePlayedCard from "@/components/PercentagePlayedCard";
+import { filterPlaysByPeriod } from "@/filters";
+
+export default {
+  name: "Stats",
+  metaInfo() {
+    return { title: this.$t("common.stats") };
+  },
+  components: {
+    DateRangeSelect,
+    PercentagePlayedCard,
+  },
+  data() {
+    return {
+      period: {
+        start: null,
+        end: null,
+      },
+    };
+  },
+  computed: {
+    ...mapGetters({
+      plays: "plays/plays",
+      tracks: "tracks/tracks",
+    }),
+    ...mapState("userSettings", ["locale"]),
+    filteredPlays() {
+      return this.plays.filter(
+        filterPlaysByPeriod(this.period.start, this.period.end)
+      );
+    },
+  },
+};
+</script>
+
+<style></style>
