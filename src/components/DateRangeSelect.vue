8191e9ba7bb6254df2d2a3f64c8c139bc285700f
Robbe Van Petegem <robbe@vanpetegem.me>
2021-12-11 11:54:54 +0100

    Allow two way binding on DateRangeSelect

--- src/components/DateRangeSelect.vue
+++ src/components/DateRangeSelect.vue
@@ -79,6 +79,12 @@ export default {
       customRange: [],
     };
   },
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    }
+  },
   computed: {
     ...mapState("userSettings", ["locale"]),
     customRangeText() {
@@ -126,6 +132,19 @@ export default {
       },
       immediate: true,
     },
+    value: {
+      handler(newValue) {
+        if (newValue.start !== this.period.start || newValue.end !== this.period.end) {
+          this.selectedPreset = "customRange";
+          // We cannot use toISOString().slice(0, 10) here, since it would ignore the users timezone
+          const start = `${newValue.start.getFullYear()}-${`${newValue.start.getMonth() + 1}`.padStart(2, "0")}-${`${newValue.start.getDate()}`.padStart(2, "0")}`;
+          const end = `${newValue.end.getFullYear()}-${`${newValue.end.getMonth() + 1}`.padStart(2, "0")}-${`${newValue.end.getDate()}`.padStart(2, "0")}`;
+          this.customRange = [start, end]
+          this.emitSelection();
+        }
+      },
+      immediate: true
+    }
   },
   methods: {
     emitSelection() {
