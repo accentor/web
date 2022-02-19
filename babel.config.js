eab0f0c0cac22d821eb2f357332fa3a78dc61ffa
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-02-19 12:51:00 +0100

    Update to vue cli 5

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ["@vue/app"],
+  presets: ["@vue/cli-plugin-babel/preset"],
   plugins: [
     "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-proposal-nullish-coalescing-operator",
