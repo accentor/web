d6e3f7cec67d046207d86708ba42552febab58d2
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-02-26 15:00:44 +0100

    Allow skipping webpack/babel/eslint cache
    This is useless in sandbox builds anyway, and specifically tries to write to
    read-only paths in nix builds.

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,23 @@ module.exports = {
       },
     },
   },
+  configureWebpack: (config) => {
+    config.cache = process.env.SKIP_CACHE !== "true";
+  },
   chainWebpack: (config) => {
+    config.module
+      .rule("js")
+      .use("babel-loader")
+      .tap((opt) =>
+        Object.assign(opt, {
+          cacheDirectory: process.env.SKIP_CACHE !== "true",
+        })
+      );
+    config
+      .plugin("eslint")
+      .tap((args) => [
+        Object.assign(args[0], { cache: process.env.SKIP_CACHE !== "true" }),
+      ]);
     ["vue-modules", "vue", "normal-modules", "normal"].forEach((match) => {
       config.module
         .rule("scss")
