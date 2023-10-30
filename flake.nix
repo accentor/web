79dd2d2fc4fdea8fbda72f5505e43197ef96ed38
Charlotte Van Petegem <charlotte@vanpetegem.me>
2023-10-30 10:49:09 +0100

    Remove unused override from devshell and add systems overrides

--- flake.nix
+++ flake.nix
@@ -3,17 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
     devshell = {
       url = "github:numtide/devshell";
       inputs = {
-        flake-utils.follows = "flake-utils";
         nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
       };
     };
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, devshell, flake-utils }:
+  outputs = { self, nixpkgs, devshell, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; overlays = [ devshell.overlays.default ]; };
