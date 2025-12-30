14680bc7f5dc666066af437f63e6583680d6c849
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-12-31 00:49:20 +0100

    Make sure the vue 2.7 version of vue-demi is used
    Due to the way vue-demi works, it doesn't work properly in a nix build. It replaces the files in `vue-demi/lib` with the relevant files for the vue version in a post-install. This doesn't happen in our nix build, and making it happen is more than annoying. By editing the vite config to always resolve to the 2.7 version, we can avoid the nix build erroring out.

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      'vue-demi': path.resolve(__dirname, './node_modules/vue-demi/lib/v2.7/'),
     }
   },
   css: {
