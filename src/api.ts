760ef7a99f4a16df2a24a594db6168fbfa754d40
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-01-02 21:58:51 +0100

    Convert api to typescript

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import { createApiClient } from "@accentor/api-client-js";
 
-let url;
+let url: string;
 if (import.meta.env.VITE_API_URL !== undefined) {
   url = import.meta.env.VITE_API_URL;
 } else if (import.meta.env.PROD) {
