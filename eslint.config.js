5eaab65cf563255a95acb471e912786179cbee19
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-01-03 15:17:21 +0100

    Lint tool config files as well

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,36 @@
+import { globalIgnores } from "eslint/config";
 import eslint from "@eslint/js";
-import globals from 'globals'
+import globals from "globals";
 import prettier from "@vue/eslint-config-prettier";
-import vue from 'eslint-plugin-vue'
-import vueParser from "vue-eslint-parser"
-import vuetify from 'eslint-plugin-vuetify'
+import vue from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
+import vuetify from "eslint-plugin-vuetify";
 
 export default [
-    ...vue.configs['flat/recommended'],
-    ...vuetify.configs['flat/recommended'],
-    eslint.configs.recommended,
-    {
-        files: ["*.vue", "**/*.vue"],
-        languageOptions: {
-            parser: vueParser
-        }
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{vue,js}"],
+  },
+  globalIgnores(["**/dist/**"]),
+  eslint.configs.recommended,
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
     },
-    prettier,
-    {
-        rules: {
-            'vue/multi-word-component-names': 'off',
-        },
-        languageOptions: {
-            sourceType: 'module',
-            globals: {
-                ...globals.browser
-            }
-        }
-    }
-]
+  },
+  ...vue.configs["flat/recommended"],
+  ...vuetify.configs["flat/recommended"],
+  prettier,
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+];
